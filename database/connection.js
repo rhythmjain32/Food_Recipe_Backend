f731f323b29b0ba67e100f418cc62a7da1f5680f
Rhythm Jain <60003625+rhythmjain32@users.noreply.github.com>
2023-01-28 01:03:21 +0530

    made db oline

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/food_recipe')
+const db_url = process.env.DATABASE_URL
+mongoose.connect(db_url)
 .then(() => {
     console.log('connection succesful');
 })
